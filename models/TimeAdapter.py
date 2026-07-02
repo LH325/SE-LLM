d8e4f0d1abe03ea5fa783f742a01de7028ca5fc1
LH325 <73454240+LH325@users.noreply.github.com>
2026-07-02 16:11:14 +0800

    Update TimeAdapter.py

--- models/TimeAdapter.py
+++ models/TimeAdapter.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+
 # Qwen
 class TimeLayer(nn.Module):
     def __init__(self, original_layer, rank=16):
@@ -27,6 +28,31 @@ def add_time_adapter(model, rank=8):
         layer.self_attn.k_proj = TimeLayer(layer.self_attn.k_proj, rank)
         layer.self_attn.v_proj = TimeLayer(layer.self_attn.v_proj, rank)
     return model
+
+# LoRA
+class TimeLayer(nn.Module):
+    def __init__(self, original_layer, rank=16):
+        super().__init__()
+        self.original_layer = original_layer
+        self.lora_A = nn.Linear(896, rank)
+        self.lora_B = nn.Linear(rank, 128)
+        nn.init.normal_(self.lora_A.weight, std=0.02)
+        nn.init.zeros_(self.lora_B.weight)
+
+    def forward(self, x):
+        original_output = self.original_layer(x)
+        A = self.lora_A(x)
+        B = self.lora_B(A)
+        return original_output + B
+
+def add_time_adapter(model, rank=8):
+    for layer in model.layers:
+        layer.self_attn.k_proj = TimeLayer(layer.self_attn.k_proj, rank)
+        layer.self_attn.v_proj = TimeLayer(layer.self_attn.v_proj, rank)
+    return model
+
+
+
 # LLama
 
 class TimeLayer(nn.Module):
